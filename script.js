ab55e7728b607c82bf08a13884b55e970f02a5c8
Minju20200305 <minju.kwon.0417@gmail.com>
2022-07-20 13:53:31 +0900

    Quote generator-need to fix

--- script.js
+++ script.js
@@ -2,33 +2,50 @@ const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
-// const newQuote = document.getElementById("new-quote");
+const newQuotBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
 
 let apiQuotes = [];
-
+console.log(apiQuotes);
 /*let을 사용하는 이유는 밑에서 value가 바뀌기 때문에 */
 
-//Show loading
+// Show loading
 function loading() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-//Hide Loading
+// //Hide Loading
 function complete() {
   quoteContainer.hidden = false;
   loader.hidden = true;
 }
 //Show New Quote
 function newQuote() {
+  loading();
   //pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
-}
 
+  //저자가 블랭크면 'Unkwon'으로 바꾸기
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //글 상자가 너무 크면 다시 크기 조정
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  //set Quote, hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+// console.log(quote);
 // Get Quotes from API
-async function getQuote() {
+async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -40,6 +57,15 @@ async function getQuote() {
   }
 }
 
+//tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, "_blank");
+}
+//Event listener
+newQuotBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 //On Load//
-getQuote();
-loading();
+getQuotes();
+
+// loading();
